86bd4040b36be6b9bb74318b969e3e16dbeee414
MarcosJRS <marcosjrs80@gmail.com>
2022-03-26 07:44:27 +0100

    Posibilidad de reemplazar los resultados con la siguiente busqueda

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,21 @@ function App() {
 
   const initialData = ['Goku'];
   const [categorias, setCategorias] = useState(initialData);
+  const [reemplazarMostrados, setReemplazarMostrados] = useState(true);
 
   const addCategoria = (categoria) => {
-    setCategorias([...categorias, categoria]);
+    const ctgorias = reemplazarMostrados ? [categoria] : [...categorias, categoria];
+    setCategorias(ctgorias);
   };
+
+  const debeRemplazar = (e) => {
+    setReemplazarMostrados(e.target.checked);
+  }
 
   return (
     <div className="App">
       <AddCategoria add={addCategoria} />
+      <input type="checkbox" onChange={debeRemplazar} checked={reemplazarMostrados} />Reemplazar los mostrados
       <Categorias categorias={categorias} />
     </div>
   );
